cae7c32a350b7c96c94edd3dffac2ae43d76b548
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-07-14 21:57:57 -0700

    Forgot to save

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
-app.listen(3000)
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+  })
+  
+
+app.get('*', (req, res) => {
+    res.render('error404')
+  })
+  
+
+app.listen(process.env.PORT)
+
---
+++ views/home.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+const Def = require('./default')
+
+function home () {
+    return (
+      <Def>
+          <main>
+              <h1>HOME</h1>
+          </main>
+          <a href="/places">
+            <button className="btn-primary">Places Page</button>
+            </a>
+
+      </Def>
+    )
+  }
+  
+
+module.exports = home
+
