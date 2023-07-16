05692cd17a90683951747ed0caeb9e0c5a9855ad
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-07-15 20:32:45 -0700

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -3,18 +3,19 @@ const router = require('express').Router()
 // GET /places
 app.get('/', (req, res) => {
     let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
+      name: 'H-Thai-ML',
+      city: 'Seattle',
+      state: 'WA',
+      cuisines: 'Thai, Pan-Asian',
+      pic: '/images/h-thai-ml-tables.png'
+    }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
+        pic: '/images/coffee-cat.jpg'
+    }]
+  
       
     res.render('places/index', { places })
 
--- views/home.jsx
+++ views/home.jsx
@@ -3,18 +3,22 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
+        <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                <div>
+                  Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                </div>
+              </div>
+              <a href="/places">
+                  <button className="btn-primary">Places Page</button>
+              </a>
           </main>
-          <a href="/places">
-            <button className="btn-primary">Places Page</button>
-            </a>
 
-      </Def>
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
-
