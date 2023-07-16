7cdeb54b8fb69c38cbeb1ee7309ee1ac795f9e5a
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-07-15 20:43:03 -0700

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -20,6 +20,11 @@ app.get('/', (req, res) => {
       res.render('places/new')
     })
     
+    router.post('/', (req, res) => {
+      console.log(req.body)
+      res.send('POST /places')
+    })
+    
     
     res.render('places/index', { places })
 
