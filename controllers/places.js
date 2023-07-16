19fbe7b5b3d8c6d77cbb83f4914ff4d43c1600f1
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-07-15 20:57:45 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,20 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
 
 // GET /places
 app.get('/', (req, res) => {
